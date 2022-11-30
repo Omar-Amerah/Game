e6218d47735a8d8fbd42ee8742201f64e60f795f
Omar Amerah <omaramerah2@gmail.com>
2022-11-30 01:48:37 +0000

    Lots of changes

--- MarioGame/main.js
+++ MarioGame/main.js
@@ -29,11 +29,15 @@ let deathBlocks
 let exit
 let numberOfCoins = 0
 let score;
-let coin;
+let coin1;
+let coin2;
+let coin3;
+let staminabar = 300;
+let staminatimeout = false; 
+let test = true
 
 
 function preload() {
-    
     //this.load.image("background", "./assets/back.png");
     this.load.spritesheet("capybara", "./assets/Capybara.png", {
         frameWidth: 64,
@@ -56,6 +60,7 @@ function create() {
     const map = this.make.tilemap({ key: "tilemap" })
     const tileset = map.addTilesetImage('base_tiles', 'base_tiles')
     const tilelayer = map.createLayer('Collide', tileset)
+    const detailLayer= map.createLayer("Details", tileset)
     deathBlocks = map.createLayer('DeathBlocks', tileset)
     exit = map.createLayer('Exit', tileset)
     
@@ -67,9 +72,17 @@ function create() {
     capybara.setScale(0.3)
     score = this.add.text(0,0, `Coins: 0`, { fontSize: '8px', fill: '#FFFFFF' })
     
-    coin = this.physics.add.sprite(310, 70, 'idlecoin').setImmovable(true)
-    coin.body.setAllowGravity(false);
-    coin.setScale(0.6)
+    coin1 = this.physics.add.sprite(150, 65, 'idlecoin').setImmovable(true)
+    coin1.body.setAllowGravity(false);
+    coin1.setScale(0.6)
+
+    coin2 = this.physics.add.sprite(880, 35, 'idlecoin').setImmovable(true)
+    coin2.body.setAllowGravity(false);
+    coin2.setScale(0.6)
+
+    coin3 = this.physics.add.sprite(1369, -10, 'idlecoin').setImmovable(true)
+    coin3.body.setAllowGravity(false);
+    coin3.setScale(0.6)
 
     blueDino.setSize(14, 16)
     blueDino.setScale(1);
@@ -116,6 +129,12 @@ function create() {
         frames: this.anims.generateFrameNumbers("idle", { start: 18, end: 23 }),
     });
     this.anims.create({
+        key: "tired",
+        frameRate: 0.1,
+        repeat: -1,
+        frames: this.anims.generateFrameNumbers("idle", { start: 14, end: 16 }),
+    });
+    this.anims.create({
         key: "coinidle",
         frameRate: 10,
         repeat: -1,
@@ -133,21 +152,34 @@ function create() {
         canJump = true;
     });
 
-    this.physics.add.collider(capybara, tilelayer)
+    //this.physics.add.collider(capybara, tilelayer)
 
     this.physics.add.collider(blueDino, deathBlocks, function() {
         blueDino.setX(180);
         blueDino.setY(70)
     })
 
-    this.physics.add.overlap(blueDino, coin, function() {
+    this.physics.add.overlap(blueDino, coin1, function() {
         numberOfCoins++
         score.setText(`Score: ${numberOfCoins}`)
-        coin.disableBody(true, true)
+        console.log(numberOfCoins)
+        coin1.disableBody(true, true)
+    })
+    this.physics.add.overlap(blueDino, coin2, function() {
+        numberOfCoins++
+        score.setText(`Score: ${numberOfCoins}`)
+        console.log(numberOfCoins)
+        coin2.disableBody(true, true)
+    })
+    this.physics.add.overlap(blueDino, coin3, function() {
+        numberOfCoins++
+        score.setText(`Score: ${numberOfCoins}`)
+        console.log(numberOfCoins)
+        coin3.disableBody(true, true)
     })
 
     this.physics.add.collider(blueDino, exit, function() {
-        if (numberOfCoins === 1) {
+        if (numberOfCoins === 3) {
             alert('You Win!!')
         }
     })
@@ -164,13 +196,40 @@ function create() {
 
 
 let enemytimer = 0
+let jumptimer = 0
 function update() {
+    if(staminabar !== 300)
+    {
+        staminabar ++;
+    }
+    if(staminabar === 0){
+        isMoving = false
+        blueDino.setVelocityX(0)
+        blueDino.setVelocityY(0)
+        test = false
+        blueDino.anims.play("tired", true);
+        staminatimeout = true
+    }
+    if(staminatimeout === true)
+    {
+        if(staminabar === 60)
+        {
+            test = true
+        }
+        if(staminabar === 120)
+        {
+            staminatimeout = false
+        }
+    }
+    
     enemytimer ++;
+    jumptimer ++;
 
     if (enemytimer < 95) {
         capybara.setVelocityX(80)
         capybara.flipX = false;
     } else if (enemytimer < 189) {
+        
         capybara.setVelocityX(-80)
         capybara.flipX = true;
     } else if (enemytimer === 190) {
@@ -181,25 +240,27 @@ function update() {
     score.x = blueDino.body.position.x - 290; 
     score.y = blueDino.body.position.y - 130;
 
-    if (shiftKey.isDown && cursors.right.isDown) {
+    if (shiftKey.isDown && cursors.right.isDown && staminabar > 0 && staminatimeout === false && test ===true) {
+        staminabar = staminabar - 3
         blueDino.setVelocityX(160);
         blueDino.flipX = false;
         blueDino.anims.play("sprint", true);
-    } else if (shiftKey.isDown && cursors.left.isDown) {
+    } else if (shiftKey.isDown && cursors.left.isDown && staminabar > 0 && staminatimeout === false && test ===true) {
+        staminabar = staminabar - 3
         blueDino.setVelocityX(-160);
         blueDino.flipX = true;
         blueDino.anims.play("sprint", true);
-    } else if (cursors.right.isDown) {
+    } else if (cursors.right.isDown&& test ===true) {
         isMoving = true;
         blueDino.flipX = false;
         blueDino.setVelocityX(90);
         blueDino.anims.play("right", true);
-    } else if (cursors.left.isDown) {
+    } else if (cursors.left.isDown&& test ===true) {
         isMoving = true;
         blueDino.flipX = true;
         blueDino.setVelocityX(-90);
         blueDino.anims.play("left", true);
-    } else {
+    } else if(test === true){
         isMoving = false;
         blueDino.anims.play("idle", true);
         blueDino.setVelocityX(0);
@@ -213,14 +274,16 @@ function update() {
         blueDino.setY(70)
     }
 
-    if (cursors.up.isDown && canJump) {
-        
+    if (cursors.up.isDown && canJump && jumptimer > 90) {
         isMoving = true;
         blueDino.anims.play("jump", true);
-        blueDino.setVelocityY(-140);
+        blueDino.setVelocityY(-145);
+        jumptimer = 0
     }
 
-    coin.anims.play("coinidle", true)
+    coin1.anims.play("coinidle", true)
+    coin2.anims.play("coinidle", true)
+    coin3.anims.play("coinidle", true)
     capybara.anims.play("capybara", true)
 
     canJump = false;
