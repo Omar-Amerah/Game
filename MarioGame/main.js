cf07e5091c4e381e018ee39862ad30616801373c
Omar Amerah <omaramerah2@gmail.com>
2022-12-01 23:40:56 +0000

    Added level 5 and 6 levels

--- MarioGame/main.js
+++ MarioGame/main.js
@@ -4,6 +4,8 @@ import Level1 from "./Level1";
 import Level2 from "./Level2";
 import Level3 from "./Level3"
 import Level4 from "./Level4"
+import Level5 from "./Level5";
+import Level6 from "./Level6";
 const game = new Phaser.Game({
     type: Phaser.AUTO,
     width: 400,
@@ -19,7 +21,7 @@ const game = new Phaser.Game({
             debug: true,
         },
     },
-    scene: [Level4, Level1, Level2, Level3],
+    scene: [Level6, Level4, Level5, Level1, Level2, Level3],
     
 });
 
