994bc5809d44f0b658563fa4f14b7b63ac9c228e
Omar Amerah <omaramerah2@gmail.com>
2022-12-02 14:41:11 +0000

    Completed game
    Co-authored-by: Arron2901 <Arron2901@users.noreply.github.com>

--- MarioGame/main.js
+++ MarioGame/main.js
@@ -4,8 +4,8 @@ import Level1 from "./Level1";
 import Level2 from "./Level2";
 import Level3 from "./Level3"
 import Level4 from "./Level4"
-import Level5 from "./Level5";
 import Level6 from "./Level6";
+import EndGame from "./endGame";
 const game = new Phaser.Game({
     type: Phaser.AUTO,
     width: 400,
@@ -21,7 +21,7 @@ const game = new Phaser.Game({
             debug: true,
         },
     },
-    scene: [Level6, Level4, Level5, Level1, Level2, Level3],
+    scene: [Level1, Level2, Level3, Level4, Level6, EndGame],
     
 });
 
